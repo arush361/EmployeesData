5dbccbe6601ad462171364a0df27f5da162ffd59
Arush Sharma <Arush361@gmail.com>
2017-02-21 03:22:04 +0530

    Task 2

--- app.js
+++ app.js
@@ -1,26 +1,59 @@
 var app = angular.module('app', ['ui.grid', 'ui.grid.cellNav']);
 
-app.controller('MainCtrl',  ['$scope', '$http', '$timeout', '$interval', 'Contents', function ($scope, $http, $timeout, $interval, Contents) {
+app.controller('MainCtrl',  ['$scope', '$http', '$timeout', '$interval', 'Contents', '$filter',
+ function ($scope, $http, $timeout, $interval, Contents, $filter) {
   
   var data = Contents;
   $scope.gridOptions = {};
-  
-  $scope.gridOptions.columnDefs = [
-    
-    { name:'id', displayName: 'Id'},    
-    { name:'name', displayName: 'Name'},
-    { name:'phone', displayName: 'Phone'},
-    { name:'address.city', displayName: 'City'},
-    { name:'address.address_line1', displayName: 'Address 1'},
-    { name:'address.address_line2', displayName: 'Address 2'},
-    { name:'address.postal_code', displayName: 'Postal Code'}
-  ];
   
   angular.forEach(data, function(obj) {
     if(isNaN(obj.phone)){
       obj.phone = "NA";
     }  
   });
-  $scope.gridOptions.data = data;
-  /*console.log(isNaN("Arush"));*/
+
+  
+     $scope.gridOptions = {
+        enableFiltering: false,
+        onRegisterApi: function(gridApi){
+          $scope.gridApi = gridApi;
+          $scope.gridApi.grid.registerRowsProcessor( $scope.singleFilter, 200 );
+      },
+      columnDefs: [
+          { field:'id', displayName: 'Id'},    
+          { field:'name', displayName: 'Name'},
+          { field:'phone', displayName: 'Phone'},
+          { field:'address.city', displayName: 'City'},
+          { field:'address.address_line1', displayName: 'Address 1'},
+          { field:'address.address_line2', displayName: 'Address 2'},
+          { field:'address.postal_code', displayName: 'Postal Code'}
+      ]
+    };
+
+   $scope.gridOptions.data = data;
+
+    $scope.filter = function() {
+      $scope.gridApi.grid.refresh();
+    };
+      
+    $scope.singleFilter = function( renderableRows ){
+      var matcher = new RegExp($scope.filterValue);
+      renderableRows.forEach( function( row ) {
+        var match = false;
+        [ 'name', 'city' ].forEach(function( field ){
+          if ((field == "name") && (row.entity[field].match(matcher))){
+            match = true;
+          }
+          else if((field == "city") && row.entity.address[field].match(matcher)) {
+            match = true;
+          }
+        });
+        if ( !match ){
+          row.visible = false;
+        }
+      });
+      return renderableRows;
+    };
+
 }]);
+
